d66f1d7848c3727908802e8fd67e469174f71cc5
Naveenthiagarajan <naveent1905@gmail.com>
2025-08-12 13:49:31 +0530

    ucommit

--- springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
+++ springapp/src/main/java/com/examly/springapp/configuration/CorsConfiguration.java
@@ -2,6 +2,7 @@ package com.examly.springapp.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -11,11 +12,12 @@ public class CorsConfiguration {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8081")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(false);
             }
         };
     }
